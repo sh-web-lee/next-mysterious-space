d0abe4276ed4379b59f39157e30747492fa93cef
lee <shwenlee@gmail.com>
2026-06-20 23:29:16 +0800

    feat: change modal content to chinese

--- src/data/modalContent.ts
+++ src/data/modalContent.ts
@@ -1,20 +1,22 @@
-export const modalContent: Record<string, { title: string; description: string }> = {
+export const modalContent: Record<
+  string,
+  { title: string; description: string }
+> = {
   goodordie: {
     title: "ZGOODorDIE",
-    description: `The "Shrine" features a spectacular array of tangible premium items capable of traversing space to drop at random. This curated collection spans all categories, showcasing benchmark products from the world's leading brands, each uniquely emblazoned with the exclusive ZGOODorDIE hallmark. Concurrently, time-limited "Mystic Realms" with diverse attributes will be unlocked periodically, materializing across premium luxury offline venues globally. Upon presenting your "Shrine" drop voucher, you will grant yourself access to these offline boutiques for a bespoke, exclusive experience. Moving forward, all partner offline venues will progressively achieve seamless, 1:1 digital-twin replication within our virtual space.`,
+    description: `"神殿"有着琳琅满目的真实物品可跨越空间并随机掉落:覆盖全品类全球头部品牌标杆好物，每件均带有ZGOODorDIE专属标识。同时不定期解锁多元特质限时秘境，落地全球高端线下实体场景。凭“神殿”掉落凭证，即可前往线下门店解锁专属体验。所有合作线下实景，后续将逐步实现空间内1:1完整复刻。`,
   },
   attitude: {
     title: "Z attitude",
     description:
-      "Within our proprietary ecosystem lie 54 Sacred Realm Keys—the scattered fragments of the Shrine's ancient codex cast down into the mortal world. Each shard bears a singular motif and a unique spiritual whisper. When they converge to form the whole once more… You, who were born divine, await only your awakening.",
+      "自有体系共有54枚神域钥匙，它是散落在人间的神典残页，每张都拥有专属图案与独有的精神密语，当它再次完整的时候......你本是神，只待觉醒",
   },
   soul: {
     title: "Z soul",
-    description:
-      "The Soul Realm—a sanctuary where you can pour out all your struggles, confusion, and life’s turning points, anytime, anywhere. Rest assured, every voice here finds an echo.",
+    description: "灵魂之境，可随时倾诉所有困境，迷茫与人生抉择，言必有回响",
   },
   wish: {
     title: "Z wish",
-    description: `A mystical, gift-shaped structure awaits. Inside, all who venture within may confide their deepest wishes and untold stories—until "You" emerge, with a tale so profound it moves the world to tears. It is then that the sanctuary will grant your every heart's desire.`,
+    description: `神秘的礼物形建筑，所有的闯入者都可以向它倾诉自己的愿望与故事，直到一个众皆动容的“你”出现。它会助“你”心想事成。`,
   },
 };
